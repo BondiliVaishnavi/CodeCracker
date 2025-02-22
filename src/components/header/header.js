bed950da04b73440cfa00b93faa5112678eeb262
BondiliVaishnavi <bondilivaishnavi61@gmail.com>
2025-02-22 12:56:37 +0530

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,29 @@
-const header=()=>{
-    return(
-        <>
-        
-        </>
-    )
+
+import React , { useState } from 'react';
+import '../header/header.css';
+import  Logo from '../../assets/images/logo_1.jpg';
+import { MdOutlineLightMode,MdOutlineDarkMode ,MdPeopleAlt } from "react-icons/md";
+const Header = () => { 
+   const [isDarkMode, setIsDarkMode] = useState(false);
+   
+   const toggleDarkMode = () => {
+      setIsDarkMode(!isDarkMode);
+      document.body.classList.toggle('dark-mode', !isDarkMode);
+    };
+    return (
+        <header className={`header ${isDarkMode ? 'dark-mode' : ''}`}>
+            <div className="header-left">
+               <a href='#' id='logo' ><img src={Logo} /></a>
+            </div>
+            <div className='header-right'>
+               <a href='#' id='profiletracker'>Profile Tracker</a>
+               <a href='#' id='aboutus'><MdPeopleAlt /> Aboutus</a>
+               <button id='mode' onClick={toggleDarkMode}>
+               {isDarkMode ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}</button>
+               <button id='login'>Login</button>
+               
+            </div>
+        </header>
+    );
 }
-export default header;
+export default Header;
